0e4b422827014cf4ff57a56fac4a533e65375ff4
Saeed Rasooli <saeed.gnu@gmail.com>
2013-10-08 14:41:07 +0330

    add igraph-test.py

---
+++ igraph-test.py
@@ -0,0 +1,35 @@
+import igraph as ig
+
+g = ig.Graph() ## one vertix
+g.vs[0]['name'] = 'A'
+
+g.add_vertices(2)
+g.vs[1]['name'] = 'B'
+g.vs[2]['name'] = 'C'
+
+
+g.add_vertices(4)
+
+g.vs[g.vcount()-1]['name'] = 'Z'
+
+print g.vs['name'] ## read only, any changes (other than assigning the whole list) is ignored
+
+
+g.add_edges([
+    (0, 1),
+    (0, 2),
+    (1, 2),
+    (3, 4),
+    (5, 6),
+])
+print '%s vertices'%g.vcount()
+print '%s edges'%g.ecount()
+
+cc = g.biconnected_components()
+print '%s connected components'%len(cc)
+for i in range(len(cc)):
+    gs = cc.subgraph(i) ## a Graph
+    print gs.get_adjacency()
+    print gs.vs['name']
+
+
